fcbae3d654371c3753fab3af912eb2f4439ee60e
Jihan Ahmed <jihanahmed41@gmail.com>
2024-01-23 07:47:31 -0800

    updating file no.3

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,3 +1,64 @@
 #include "sort.h"
 
-void quick_sort(int *array, size_t size);
+/**
+ * lomuto - function to partition the passed array
+ *
+ * @array: the array to be partitiones
+ * @lower_bound: the first index of the array
+ * @upper_bound: the max index of the array
+ * @size: the size if the array
+ */
+
+void lomuto(int *array, int lower_bound, int upper_bound, size_t size)
+{
+	int pivot = array[upper_bound];
+	int i, j = lower_bound;
+
+	if (lower_bound > upper_bound)
+		return;
+	for (i = lower_bound; i < upper_bound; i++)
+	{
+		if (array[i] <= pivot)
+		{
+			if (array[i] != array[j])
+			{
+				swap_int(&array[i], &array[j]);
+				print_array(array, size); }
+			j++;
+		}
+	}
+	if (array[j] != array[upper_bound])
+	{
+		swap_int(&array[j], &array[upper_bound]);
+		print_array(array, size); }
+	lomuto(array, lower_bound, j - 1, size);
+	lomuto(array, j + 1, upper_bound, size);
+}
+
+/**
+ *swap_int - swaps two integer
+ *
+ * @num1: address of the first number
+ * @num2: address of the second number
+ */
+
+void swap_int(int *num1, int *num2)
+{
+	int temp = *num1;
+
+	*num1 = *num2;
+	*num2 = temp;
+}
+
+/**
+ * quick_sort - function that sorts array ascendengly using quick sort algorith
+ *
+ * @array: the array to be sorted
+ * @size: the size of the array
+ */
+void quick_sort(int *array, size_t size)
+{
+	if (array == NULL || size < 2)
+		return;
+	lomuto(array, 0, size - 1, size);
+}
--- sort.h
+++ sort.h
@@ -1,9 +1,7 @@
-
-#ifndef MAIN_h
-#define MAIN_h
-
+#ifndef SORT_H
+#define SORT_H
+#include <stddef.h>
 #include<stdio.h>
-#include <stdlib.h>
 /**
  * struct listint_s - Doubly linked list node
  *
@@ -18,10 +16,13 @@ typedef struct listint_s
 	struct listint_s *next;
 } listint_t;
 
+void swap_int(int *num1, int *num2);
+
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
-
+void lomuto(int *array, int lower_bound, int upper_bound, size_t size);
+void quick_sort(int *array, size_t size);
 #endif
