6a3830cc53f0a503c7b0e4dcc853697a4c71f026
foash-111 <mr2221@fayoum.edu.eg>
2024-01-23 13:36:25 +0200

    sorting_algorithm...bubble, insertion, selection sort

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -11,7 +11,7 @@ void insertion_sort_list(listint_t **list)
 	listint_t *ptr, *p_ptr, *temp;
 
 	if (list == NULL || *list == NULL || (*list)->next == NULL)
-		return;
+	return;
 	ptr = (*list)->next;
 	while (ptr != NULL)
 	{
