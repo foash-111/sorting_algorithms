3e63ed6738d08d12ba949ee93e6da44c68a060f6
foash-111 <mr2221@fayoum.edu.eg>
2024-01-24 10:11:19 +0200

    sorting_algorithm...bubble, insertion, selection sort

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,42 +1,85 @@
-#include "sort.h"
+#include"sort.h"
 
 /**
- * insertion_sort_list - sorts a doubly linked list of integers using insertion
- *
- * @list: the head of the list
- */
+ * swap_nodes - swap between 2 nodes
+ * @first_node: first number
+ * @second_node: second number
+ * Return: (nothing)
+*/
+
+void swap_nodes(listint_t *first_node, listint_t *second_node)
+{
+	if (first_node->prev == NULL && second_node->next == NULL)
+	{
+		second_node->next = first_node;
+		second_node->prev = NULL;
+		first_node->prev = second_node;
+		first_node->next = NULL;
+	}
+	else if (first_node->prev == NULL && second_node->next != NULL)
+	{
+		first_node->next = second_node->next;
+		second_node->next->prev = first_node;
+		first_node->prev = second_node;
+		second_node->next = first_node;
+		second_node->prev = NULL;
+	}
+	else if (first_node->prev != NULL && second_node->next == NULL)
+	{
+		second_node->prev = first_node->prev;
+		first_node->prev->next = second_node;
+		first_node->next = NULL;
+		first_node->prev = second_node;
+		second_node->next = first_node;
+	}
+	else if (first_node->prev != NULL && second_node->next != NULL)
+	{
+		first_node->prev->next = first_node->next;
+		second_node->prev = first_node->prev;
+		first_node->next = second_node->next;
+		first_node->prev = second_node;
+		second_node->next = first_node;
+		first_node->next->prev = first_node;
+	}
+
+}
+
+/**
+ * insertion_sort_list - insertion_sort algorithm emplementing on linked list
+ * @list: our pointer to our double linked list
+ * Return(nothing)
+*/
 
 void insertion_sort_list(listint_t **list)
 {
-	listint_t *ptr, *p_ptr, *temp;
+	listint_t *head = *list;
+	listint_t *current = *list;
+	int key = 0, flag = 0;
 
-	if (list == NULL || *list == NULL || (*list)->next == NULL)
+	if (head == NULL || head->next == NULL)
 	return;
-	ptr = (*list)->next;
-	while (ptr != NULL)
+	if (head != NULL)
+	head = head->next;
+
+	while (head)
 	{
-		p_ptr = ptr->prev;
-		while (p_ptr != NULL && ptr->n < p_ptr->n)
+		key = head->n;
+		current = head->prev;
+		while ((current) && (current->n > key))
 		{
-			temp = p_ptr->next;
-			p_ptr->next = ptr->next;
-			ptr->next = temp;
-			if (ptr->next != NULL)
-				ptr->next->prev = ptr;
-			if (p_ptr->next != NULL)
-				p_ptr->next->prev = p_ptr;
-			temp = p_ptr->prev;
-			p_ptr->prev = ptr->prev;
-			ptr->prev = temp;
-			if (ptr->prev != NULL)
-				ptr->prev->next = ptr;
-			else
-				(*list) = ptr;
-			if (p_ptr->prev != NULL)
-				p_ptr->prev->next = p_ptr;
-			p_ptr = ptr->prev;
-			print_list(*list);
+			swap_nodes(current, current->next);
+			if (current->prev->prev == NULL && (current->n > key))
+			(*list) = current->prev;
+			if (flag == 0)
+			{
+				head = head->next;
+				flag = 1;
+			}
+			print_list((const listint_t *)*list);
+
+			current = current->prev->prev;
 		}
-		ptr = ptr->next;
+		head = head->next;
+		flag = 0;
 	}
 }
